5da451f550ab9392c1b127af3b81b874558d5672
marquexmar <lunagonzalesmarco@gmail.com>
2020-10-02 09:26:53 -0400

    modificacion del chart

--- graficos/chartIngresosTendenciaVarios.php
+++ graficos/chartIngresosTendenciaVarios.php
@@ -1,19 +1,17 @@
-<style type="text/css">
-
-#chart-container7 {
-    width: 100%;
-}
-</style>
 <?php
-
 //session_start();
-
 //$fondoTemporal='1011|1020';//echo "hola como vamos";
-
 //
 $fila=$_SESSION["filaTemporal"];
 
 ?>
+<style type="text/css">
+
+#chart-container<?=$fila;?> {
+    width: 100%;
+}
+</style>
+
 <script>var filaChart=<?=$fila?>;</script>
 <script type="text/javascript" src="../assets/chartjs/js/jquery.min.js"></script>
 <script type="text/javascript" src="../assets/chartjs/js/Chart.bundle.js"></script>
@@ -22,8 +20,8 @@ $fila=$_SESSION["filaTemporal"];
 
 </head>
 
-    <div id="chart-container7" style="margin-left:auto; margin-right:auto">
-        <canvas id="graphCanvas7<?=$fila?>"></canvas>
+    <div id="chart-container<?=$fila?>" style="margin-left:auto; margin-right:auto">
+        <canvas id="graphCanvas<?=$fila?>"></canvas>
     </div>
 
     <script>
@@ -57,28 +55,28 @@ $fila=$_SESSION["filaTemporal"];
                         labels: mes,
                         datasets: [
                             {
-                                label: 'Pres. GA',
+                                label: 'Pres. Gestión Anterior',
                                 backgroundColor: "rgba(241, 227, 17, 0.2)",
                                 borderColor: "rgb(241, 227, 17)",
                                 borderWidth:2,
                                 data: montoPresIngresoAnt
                             },
                             {
-                                label: 'Ej. GA',
+                                label: 'Ej. Gestión Anterior',
                                 backgroundColor: "rgba(16, 242, 47, 0.2)",
                                 borderColor: "rgb(16, 242, 47)",
                                 borderWidth:2,
                                 data: montoEjIngresoAnt
                             },
                             {
-                                label: 'Pres.Area',
+                                label: 'Presupuesto',
                                 backgroundColor: "rgba(255, 99, 132, 0.2)",
                                 borderColor: "rgb(255, 99, 132)",
                                 borderWidth:2,
                                 data: montoPresIngreso
                             },
                             {
-                                label: 'Ej.Area',
+                                label: 'Ejecutado',
                                 backgroundColor: "rgba(75, 192, 192, 0.2)",
                                 borderColor: "rgb(75, 192, 192)",
                                 borderWidth:2,
@@ -87,7 +85,7 @@ $fila=$_SESSION["filaTemporal"];
                         ]
                     };
 
-                    var graphTarget = $("#graphCanvas7"+filaChart);
+                    var graphTarget = $("#graphCanvas"+filaChart);
 
                     var barGraph = new Chart(graphTarget, {
                         type: 'line',
